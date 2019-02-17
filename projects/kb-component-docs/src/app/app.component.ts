97715cc86c9925a11541b79445c57bd32b485696
Neil Kalman <neilkalman@gmail.com>
2019-02-17 04:01:13 +0200

    tidy up the homepage and add extra details

--- projects/kb-component-docs/src/app/app.component.ts
+++ projects/kb-component-docs/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'kb-docs-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  version: string = environment.VERSION;
   title = 'kb-component';
+  html = `
+<kb-highlighted-text [needle]="searchTerm"
+                     [haystack]="content">
+</kb-highlighted-text>
+  `.trim();
+  ts = `
+export class AppComponent {
+  searchTerm = 'hello';
+  content = 'This is a nice hello world text. this is case insensitive: Hello';
+}
+  `.trim();
+  install = 'npm install --save @kibibit/kb-components';
+  include = `
+/ *.module.ts
+// ...
+import { KbComponentsModule } from '@kibibit/kb-components';
+
+@NgModule({
+  // ...
+  imports: [
+    // ...
+    KbComponentsModule // <======= ADD THIS
+  ],
+  // ...
+})
+export class AppModule { }
+  `.trim();
 }
--- projects/kb-component-docs/src/app/app.module.ts
+++ projects/kb-component-docs/src/app/app.module.ts
@@ -10,11 +10,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
+import { LogoComponent } from './logo/logo.component';
 
+import { HighlightModule } from 'ngx-highlightjs';
+
+import xml from 'highlight.js/lib/languages/xml';
+import scss from 'highlight.js/lib/languages/scss';
+import typescript from 'highlight.js/lib/languages/typescript';
+
+/**
+ * Import every language you wish to highlight here
+ * NOTE: The name of each language must match the file name its imported from
+ */
+export function hljsLanguages() {
+  return [
+    { name: 'typescript', func: typescript },
+    { name: 'scss', func: scss },
+    { name: 'xml', func: xml }
+  ];
+}
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +45,10 @@ import { MatCardModule } from '@angular/material/card';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    HighlightModule.forRoot({
+      languages: hljsLanguages
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
